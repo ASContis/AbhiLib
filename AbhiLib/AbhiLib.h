f84394adb16209d8cc88eea18b0a59f4364cf851
ASContis <abhijit.soni@contis.com>
2022-04-18 16:35:40 +0530

    Initial project setup

---
+++ AbhiLib/AbhiLib.h
@@ -0,0 +1,18 @@
+//
+//  AbhiLib.h
+//  AbhiLib
+//
+//  Created by Abhijit Soni on 18/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AbhiLib.
+FOUNDATION_EXPORT double AbhiLibVersionNumber;
+
+//! Project version string for AbhiLib.
+FOUNDATION_EXPORT const unsigned char AbhiLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AbhiLib/PublicHeader.h>
+
+
