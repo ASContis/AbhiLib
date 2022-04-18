0c41a8fc0827c6cb91e76103a95a4eb859cf6480
ASContis <abhijit.soni@contis.com>
2022-04-18 16:38:58 +0530

    Added test cases

---
+++ AbhiLib/AbhiLab.swift
@@ -0,0 +1,21 @@
+//
+//  AbhiLab.swift
+//  AbhiLib
+//
+//  Created by Abhijit Soni on 18/04/22.
+//
+
+import Foundation
+public final class AbhiLab {
+
+    let name = "AbhiLab"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
